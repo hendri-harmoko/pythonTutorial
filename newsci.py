32c40cf1ba96d1f07db1716ed9e1d5a175d62f5f
Hendri-Harmoko <hendri.harmoko@gmail.com>
2023-08-16 04:50:18 +0800

    Copying openfile.py to newsci.py

---
+++ newsci.py
@@ -0,0 +1,17 @@
+# Initialize my data variable
+data = []
+
+# Read the data file
+filename = "data/wxobs20170821.txt"
+
+with open(filename, 'r') as datafile:
+    for _ in range(3):
+        datafile.readline()
+        
+    for line in datafile:
+        datum = line.split()
+        data.append(datum)
+
+# DEBUG
+# Latest Version
+
